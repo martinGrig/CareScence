efa879ea3cb0f6b5b8b439916c9b00b715684581
JD Destine <d.destine@student.fontys.nl>
2019-12-13 09:11:07 +0100

    overview

--- CareScence/CareScence/ViewController.swift
+++ CareScence/CareScence/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import WebKit
+
+
+class ExerciseCell: UITableViewCell{
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
+         
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let myUrl = URL(string: "https://www.youtube.com/watch?v=0bmE9XY3sOc")!
+                
+             let myRequest = URLRequest(url:myUrl)
+           //  webkit.load(myRequest)
+            
+            
+            
+            }
+        
+        
+    
+
+
     }
-
-
-}
 
