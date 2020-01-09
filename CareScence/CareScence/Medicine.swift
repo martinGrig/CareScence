61190e1dfcb852daf7e8bf9ddd7989cbb3be103c
Ivanov,Dimitar D.A <dimitar.ivanov@student.fontys.nl>
2020-01-09 15:05:34 +0100

    Medicine

---
+++ CareScence/CareScence/Medicine.swift
@@ -0,0 +1,18 @@
+//
+//  Medicine.swift
+//  CareScence
+//
+//  Created by Student on 09.01.2020 г..
+//  Copyright © 2020 JD Destine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Medicine{
+    let name: String
+    let day: String
+    let time: String
+    let frequency: String
+    
+}
