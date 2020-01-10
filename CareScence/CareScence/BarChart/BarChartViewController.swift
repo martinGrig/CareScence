dbde96d87e5eb00ce9ec9dc008815ebafaa42c68
JD Destine <d.destine@student.fontys.nl>
2020-01-10 15:48:31 +0100

    Added bar chart and organised files

---
+++ CareScence/CareScence/BarChart/BarChartViewController.swift
@@ -0,0 +1,70 @@
+//
+//  BarChartViewController.swift
+//  CareScence
+//
+//  Created by JD Destine on 10/01/2020.
+//  Copyright © 2020 JD Destine. All rights reserved.
+//
+
+import UIKit
+
+class BarChartViewController: UIViewController {
+
+
+    @IBOutlet weak var basicBarChart: BasicBarChart!
+    
+    
+      
+    
+    private let numEntry = 20
+        
+    override func viewDidAppear(_ animated: Bool) {
+          let dataEntries = generateEmptyDataEntries()
+          basicBarChart.updateDataEntries(dataEntries: dataEntries, animated: false)
+          
+          let timer = Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) {[unowned self] (timer) in
+            let dataEntries = self.generateRandomDataEntries();              self.basicBarChart.updateDataEntries(dataEntries: dataEntries, animated: true)
+          }
+          timer.fire()
+      }
+      
+        
+        func generateEmptyDataEntries() -> [DataEntry] {
+            var result: [DataEntry] = []
+            Array(0..<numEntry).forEach {_ in
+                result.append(DataEntry(color: UIColor.clear, height: 0, textValue: "0", title: ""))
+            }
+            return result
+        }
+        
+        func generateRandomDataEntries() -> [DataEntry] {
+            let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
+            var num = 0;
+            var result: [DataEntry] = []
+            var value = 500
+            for i in 0..<numEntry {
+                if i > 0{
+                    value = result[i-1].textValue.count + 500
+                }else{  value = 5000}
+                
+
+            
+                if (5000 / 2) > value{
+                    num = 5
+                }else if (5000 / 2) < value{
+                    num = 3;
+                }else if (value > 5000){
+                    num = 0;
+                }
+                let height: Float = Float(value) / 1000.0
+                
+                let formatter = DateFormatter()
+                formatter.dateFormat = "d MMM"
+                var date = Date()
+                date.addTimeInterval(TimeInterval(24*60*60*i))
+                result.append(DataEntry(color: colors[num], height: height, textValue: "\(value)", title: formatter.string(from: date)))
+            }
+            return result
+        }
+    }
+
---
+++ CareScence/CareScence/BarChart/DataEntry.swift
@@ -0,0 +1,23 @@
+//
+//  DataEntry.swift
+//  CareScence
+//
+//  Created by JD Destine on 10/01/2020.
+//  Copyright © 2020 JD Destine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DataEntry {
+    let color: UIColor
+    
+    /// Ranged from 0.0 to 1.0
+    let height: Float
+    
+    /// To be shown on top of the bar
+    let textValue: String
+    
+    /// To be shown at the bottom of the bar
+    let title: String
+}
