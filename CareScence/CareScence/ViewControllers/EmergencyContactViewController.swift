a76a5420f90390d4cfe66102c7297aaf8777a3e6
Martin Grigorov <m.grigorov@student.fontys.nl>
2020-01-16 16:01:19 +0100

    call function fixed

--- CareScence/CareScence/ViewControllers/EmergencyContactViewController.swift
+++ CareScence/CareScence/ViewControllers/EmergencyContactViewController.swift
@@ -14,6 +14,8 @@ class EmergencyContactViewController: UIViewController {
     @IBOutlet weak var EmailLabel: UILabel!
     @IBOutlet weak var PhoneLabel: UILabel!
     
+    fileprivate var application = UIApplication.shared
+    
 
     var emergency = EmergencyContact(name: "Jane Smith", email: "jane@abv.cr", photo: #imageLiteral(resourceName: "emergency-contact"), phoneNumber: "+31 6 12345678")
     override func viewDidLoad() {
@@ -21,36 +23,19 @@ class EmergencyContactViewController: UIViewController {
 
     }
     
-    @IBAction func CallEmergency(_ sender: Any) {
-        emergency.phoneNumber.makeAColl()
+    @IBAction func CallTpped(_ sender: Any) {
+        
+        if let phoneURL = URL(string: "tel://0031612345678"){
+            if application.canOpenURL(phoneURL){
+                application.open(phoneURL, options:  [:], completionHandler: nil)
+                print("works")
+            }else{
+//                alert
+            }
+        }
     }
     
 
-}
-extension String {
-
-    enum RegularExpressions: String {
-        case phone = "^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$"
-    }
-
-    func isValid(regex: RegularExpressions) -> Bool { return isValid(regex: regex.rawValue) }
-    func isValid(regex: String) -> Bool { return range(of: regex, options: .regularExpression) != nil }
-
-    func onlyDigits() -> String {
-        let filtredUnicodeScalars = unicodeScalars.filter { CharacterSet.decimalDigits.contains($0) }
-        return String(String.UnicodeScalarView(filtredUnicodeScalars))
-    }
-
-    func makeAColl() {
-        guard   isValid(regex: .phone),
-                let url = URL(string: "tel://\(self.onlyDigits())"),
-                UIApplication.shared.canOpenURL(url) else { return }
-        if #available(iOS 10, *) {
-            UIApplication.shared.open(url)
-        } else {
-            UIApplication.shared.openURL(url)
-        }
-    }
 }
 
 extension EmergencyContactViewController{
