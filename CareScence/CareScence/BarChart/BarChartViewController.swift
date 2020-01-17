4926588aaa88abf5872be947ada2364234a8152e
Martin Grigorov <m.grigorov@student.fontys.nl>
2020-01-17 11:18:09 +0100

    constraints added on battery page

--- CareScence/CareScence/BarChart/BarChartViewController.swift
+++ CareScence/CareScence/BarChart/BarChartViewController.swift
@@ -41,7 +41,7 @@ class BarChartViewController: UIViewController {
             }
              
              p = p + 1
-            if(p == 3){
+            if(p == 2){
                 
                 timer.invalidate()
                 check = true;
