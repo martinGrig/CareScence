8e04b79ea286da14a71695424983c5cbb986a068
Dewi,Angelica <a.dewi@student.fontys.nl>
2020-01-17 16:09:19 +0100

    Fixed walking analysis page 2. Changed bar colors in walking analysis page 1.

--- CareScence/CareScence/BarChart/BarChartViewController.swift
+++ CareScence/CareScence/BarChart/BarChartViewController.swift
@@ -87,7 +87,7 @@ class BarChartViewController: UIViewController {
 
             
             func generateRandomDataEntries() -> [DataEntry] {
-                let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
+                let colors = [#colorLiteral(red: 0.7073796988, green: 0.916798532, blue: 0.3351904154, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 1, green: 0.8496686816, blue: 0, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8415471315, green: 0.3140499294, blue: 0.3379223943, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
                            var num = 0;
                            var result: [DataEntry] = []
                            var value = 500
---
+++ CareScence/CareScence/ViewControllers/WalkAnalysisViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WalkAnalysisViewController.swift
+//  CareScence
+//
+//  Created by Student on 1/17/20.
+//  Copyright © 2020 JD Destine. All rights reserved.
+//
+
+import UIKit
+
+class WalkAnalysisViewController: UIViewController {
+
+    @IBAction func howToFixButton(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 2
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        
+        
+    
+    
+}
